c33c65b644c55e3c0e2e0af000955519c6b2ff20
Juhasen <juszczyk-krystian@wp.pl>
2025-06-06 11:05:12 +0200

    Add additional dependencies for Compose Material Icons, DocumentFile, and Navigation

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,9 +16,9 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") } // Dodaję repozytorium jitpack
     }
 }
 
 rootProject.name = "TODO"
 include(":app")
- 
